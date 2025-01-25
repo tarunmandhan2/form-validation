9cddcd3745d3fc55df281863c12a89e69c9b09fc
Tarun 2 <141573811+tarunmandhan2@users.noreply.github.com>
2025-01-25 19:03:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>14. Create a Reusable Form Component with Validation </h2>
+      <h4>
+        Problem: Build a reusable form component that handles form state and
+        validation for various form fields.
+      </h4>
+    </div>
+  );
+}
